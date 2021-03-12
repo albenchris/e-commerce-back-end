18eb099a962301676d23128e8e06ae490eb2e743
albenchris <albenchris00@gmail.com>
2021-03-12 17:46:15 -0600

    function to delete a tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -111,7 +111,22 @@ router.put('/:id', (req, res) => {
 
 // delete a tag
 router.delete('/:id', (req, res) => {
-
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({ message: "No tags found with that id!" });
+        return;
+      }
+      res.json(dbTagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
