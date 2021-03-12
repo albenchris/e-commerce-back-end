b4b7b4c44a8213b448d6f70f6502d0581bea0c26
albenchris <albenchris00@gmail.com>
2021-03-12 14:35:59 -0600

    function to find product by id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,13 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   Product.findAll({
+    attributes: [
+      "id",
+      "product_name",
+      "price",
+      "stock",
+      "category_id"
+    ],
     include: [
       {
         model: Category,
@@ -35,6 +42,45 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      "id",
+      "product_name",
+      "price",
+      "stock",
+      "category_id"
+    ],
+    include: [
+      {
+        model: Category,
+        attributes: [
+          "id",
+          "category_name"
+        ]
+      },
+      {
+        model: Tag,
+        attributes: [
+          "id",
+          "category_name"
+        ]
+      }
+    ]
+  })
+    .then(dbProductData => {
+      if (!dbProductData) {
+        res.status(404).json({ message: "No products found with that id!" });
+        return;
+      }
+      res.json(dbProductData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // create new product
