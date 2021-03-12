8be22054bced465f77e858c4d77cf2c89e49e385
albenchris <albenchris00@gmail.com>
2021-03-12 08:31:13 -0600

    Tag model columns defined

--- models/Tag.js
+++ models/Tag.js
@@ -6,7 +6,20 @@ class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        // at least 3 characters
+        len: [3]
+      }
+    }
   },
   {
     sequelize,
